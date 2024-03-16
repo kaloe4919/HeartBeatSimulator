a8ea46fc569e36ab5911a5f77cb2735a933caadb
YamikarasuMk22 <yamikarasumk22@gmail.com>
2024-03-17 03:00:31 +0900

    feat: add motion of ventricle and atria

--- data/others/simulator/init.js
+++ data/others/simulator/init.js
@@ -17,6 +17,8 @@ TYRANO.kag.hbsim = {
       heartRate: 65,
       heartRateMin: 60,
       heartRateMax: 70,
+      // used for arrhythmias that cause differences in heart rate of the ventricles and atria (VT, AF, etc)
+      atrialHeartRate: 65,
       // variable increase by rapid changes in heart rate
       burden: 0,
       // variable increase by changes in emotions
